c98c0495f49d9b69e099865aaa0ce89b544271d3
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-29 20:53:44 +0530

    new branch 5

--- modules/subnet/outputs.tf
+++ modules/subnet/outputs.tf
@@ -6,3 +6,12 @@ output "subnet-out-2" {
     value = aws_subnet.sub-stage-2
   
 }
+output "aws-internet-gateway" {
+    value = aws_internet_gateway.stage-igw.id
+  
+}
+
+output "stage-rtb" {
+    value = aws_route_table.stage-rtb.id
+  
+}
