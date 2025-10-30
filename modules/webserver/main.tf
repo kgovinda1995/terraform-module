9549c49d7e9cace83f118d4d75b53a94e0a14d86
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-30 08:28:40 +0530

    new branch 22

--- modules/webserver/main.tf
+++ modules/webserver/main.tf
@@ -57,7 +57,7 @@ resource "aws_key_pair" "my-terraform-1" {
    public_key = file(var.public_key_location)
   
 }
-resource "aws_instance" "stage-server-1" {
+resource "aws_instance" "stage-server" {
     ami = data.aws_ami.latest-ubuntu-image.id
     instance_type = var.instance_type[0] 
 
