63485f23334143de171bde68f354bfdf6e831136
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-30 07:34:36 +0530

    new branch 13

--- modules/webserver/variables.tf
+++ modules/webserver/variables.tf
@@ -2,8 +2,10 @@ variable vpc_id { }
 variable my_ip { }
 variable availability_zone {}
 variable environment {}  
-variable instance_type {}
 variable subnet_id {}
 variable public_key_location {}
 variable private_key_location {}
 variable image_name {}
+variable "instance_type" {
+  type = list(string)
+}
