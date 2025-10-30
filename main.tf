208f717501baf1d06fb3bea5456d61e44302f37d
kgovinda1995 <kgovinda.mech@gmail.com>
2025-10-30 14:01:20 +0530

    remote module added 5

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = "value"
+  backend "s3" {
+   bucket  = "terrform-bucket"  # bucket need to create
+   key = "myapp/state.tfstate"
+    
+  }
+}
+
 provider "aws" {
     region = "ap-south-1"
     access_key = "AKIAZ3OZYLMB3VYYP7MS"
